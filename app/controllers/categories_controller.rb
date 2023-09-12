3fc77a59cd8283da704ca41b62ecc2056c2e0789
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 00:06:52 +0300

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,11 +4,13 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.includes(:purchases)
   end
 
   # GET /categories/1 or /categories/1.json
-  def show; end
+  def show
+    @purchases = @category.purchases.order(created_at: :desc)
+  end
 
   # GET /categories/new
   def new
@@ -25,7 +27,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
