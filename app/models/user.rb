4055e01198822388a3d58d27aba96a883a1d1838
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-12 14:01:58 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many :categories
+  has_many :purchases
+
+  # Attributes
+  attribute :name, :string
+
+  # Callbacks
+
+  # Validations
+  validates :name, presence: true
+
+  # Mehtods
 end
