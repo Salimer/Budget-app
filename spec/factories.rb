e1ac8217bf37a3f938f9dd2c9efec773a1096a1a
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 18:34:00 +0300

    Update factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,28 +1,26 @@
 FactoryBot.define do
-
-    factory :user, aliases: [ :author ] do
-      sequence(:name) { |n| "User #{n}" }
-      email { Faker::Internet.email }
-      password { 'password123' }
-    end
-
-    factory :category do
-        sequence(:name) { |n| "Category #{n}" }
-        icon { 'https://example.com/icon.png' }
-        user
-      end
-
-      factory :purchase do
-        sequence(:name) { |n| "Purchase #{n}" }
-        amount { Faker::Number.decimal(l_digits: 2) }
-        author
-    
-        trait :with_category do
-          after(:create) do |purchase|
-            purchase.categories << create(:category) # Assuming :category is your category factory
-          end
-        end
-      end
-      factory :purchase_with_category, traits: [:with_category]
+  factory :user, aliases: [:author] do
+    sequence(:name) { |n| "User #{n}" }
+    email { Faker::Internet.email }
+    password { 'password123' }
   end
-  
+
+  factory :category do
+    sequence(:name) { |n| "Category #{n}" }
+    icon { 'https://example.com/icon.png' }
+    user
+  end
+
+  factory :purchase do
+    sequence(:name) { |n| "Purchase #{n}" }
+    amount { Faker::Number.decimal(l_digits: 2) }
+    author
+
+    trait :with_category do
+      after(:create) do |purchase|
+        purchase.categories << create(:category) # Assuming :category is your category factory
+      end
+    end
+  end
+  factory :purchase_with_category, traits: [:with_category]
+end
