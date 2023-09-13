0ba1c28ee5a0e8b0c105dd0f94f8ddb12b7e8679
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 13:04:01 +0300

    Create category_purchases_spec.rb

---
+++ spec/requests/category_purchases_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe '/purchases', type: :request do
+  describe 'GET /index' do
+    before do
+      @category = FactoryBot.create(:category)
+      @purchase = FactoryBot.build(:purchase)
+      @purchase.categories << @category
+      @category.purchases << @purchase
+    end
+    before(:each) { sign_in @category.user }
+
+    it 'returns a successful response' do
+      get category_path(@category)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'correct template is rendered' do
+      get category_path(@category)
+      expect(response).to render_template(:show)
+    end
+
+    it 'the response body includes correct placeholder text' do
+      get category_path(@category)
+      expect(response.body).to include('Amount')
+    end
+  end
+end
