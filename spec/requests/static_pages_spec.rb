eee1bb62eab3ef3a8a1c136c0fc919067afaf364
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 18:33:51 +0300

    Update static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /splash" do
-    it "returns http success" do
+RSpec.describe 'StaticPages', type: :request do
+  describe 'GET /splash' do
+    it 'returns http success' do
       get root_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
