c07e9bd9aeefbdcc18e02a22c8072c60281b76b0
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 18:34:09 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#splash'
   resources :categories do
-    resources :purchases, only: [:new]
+    resources :purchases, only: [:new, :show]
   end
-  resources :purchases, except: [:new]
+  resources :purchases, except: [:new, :show]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
