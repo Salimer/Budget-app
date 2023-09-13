239cc0f8542f5d4ee05fbfc65eab91eb2f311b80
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 13:03:51 +0300

    Update category_spec.rb

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { create(:category) }
+  it 'validity' do
+    expect(category).to be_valid
+  end
+
+  it 'has a name' do
+    category.name = ''
+    expect(category).not_to be_valid
+  end
+
+  it 'has a user associated' do
+    category.user = nil
+    expect(category).not_to be_valid
+  end
+
+  it 'associated user is valid' do
+    expect(category.user).to be_valid
+  end
 end
