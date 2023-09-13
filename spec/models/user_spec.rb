856b8fd018dab204e64f395d7607a87851cb1ae7
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-13 13:03:55 +0300

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  it 'validity' do
+    expect(user).to be_valid
+  end
+
+  it 'has a name' do
+    user.name = ''
+    expect(user).not_to be_valid
+  end
 end
