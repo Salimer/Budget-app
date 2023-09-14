85f73d96571b3ecfa8602d94ba5f3ec5eeb5ff15
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-14 13:04:03 +0300

    Update category_purchases_spec.rb

--- spec/requests/category_purchases_spec.rb
+++ spec/requests/category_purchases_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe '/purchases', type: :request do
 
     it 'the response body includes correct placeholder text' do
       get category_path(@category)
-      expect(response.body).to include('Amount')
+      expect(response.body).to include('New purchase')
     end
   end
 end
